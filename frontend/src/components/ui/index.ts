aae7220c56c6b3665d48b9d977d01914685a6ac8
Sean McDonnell <seanebonesz@gmail.com>
2025-10-21 00:45:51 -0500

    Add UI components index for better module resolution in Vercel build

---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,17 @@
+/**
+ * UI Components Index
+ * Central export point for all UI components
+ */
+
+export * from './badge';
+export * from './button';
+export * from './card';
+export * from './input';
+export * from './label';
+export * from './select';
+export * from './switch';
+export * from './tabs';
+export * from './textarea';
+export * from './toast';
+export * from './toaster';
+
